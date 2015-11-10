44fd05e29c4e7c6b8e0278b59d6cff3d0d6d5113
Korrawe Karunratanakul <korrawe@gmail.com>
2015-11-10 22:33:53 +0700

    Added InputUtility

---
+++ Dragoniquarium/src/input/InputUtility.java
@@ -0,0 +1,16 @@
+package input;
+
+public class InputUtility {
+	
+	private static boolean keyPressed[] = new boolean[256];
+	
+	public static boolean getKeyPressed(int key) {
+		if(key < 0 || key >= 256) return false;
+		return keyPressed[key];
+	}
+	
+	public static void setKeyPressed(int key, boolean pressed) {
+		if(key < 0 || key >= 256) return;
+		keyPressed[key] = pressed;
+	}
+}
