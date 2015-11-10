29782d2d8cc57d1133254cd91bbffe667901cd04
Korrawe Karunratanakul <korrawe@gmail.com>
2015-11-10 23:11:43 +0700

    Added Resource

---
+++ Dragoniquarium/src/render/Resource.java
@@ -0,0 +1,32 @@
+package render;
+
+import java.applet.Applet;
+import java.applet.AudioClip;
+import java.awt.Font;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+
+public class Resource {
+	
+	public static final Font standardFont = new Font("Tahoma", Font.BOLD, 30);
+	public static BufferedImage egg1Sprite;
+	public static AudioClip coinSound;
+	
+	static {
+		try {
+			
+			ClassLoader loader = Resource.class.getClassLoader();
+			egg1Sprite = ImageIO.read(loader.getResource("res/Egg1.png"));
+			coinSound = Applet.newAudioClip((loader.getResource("res/coin.wav")).toURI().toURL());
+			
+		} catch (Exception e) {
+			egg1Sprite = null;
+			coinSound = null;
+		}
+		
+		
+	}
+	
+	
+}
