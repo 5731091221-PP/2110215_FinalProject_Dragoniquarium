a30664f53c38d316c980b4a94b571290626f76ff
Korrawe Karunratanakul <korrawe@gmail.com>
2015-11-15 20:37:10 +0700

    Updated keyTrigger and keyListener

--- Dragoniquarium/src/render/GameScreen1.java
+++ Dragoniquarium/src/render/GameScreen1.java
@@ -8,6 +8,9 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 
 import javax.swing.JComponent;
 
@@ -21,8 +24,61 @@ public class GameScreen1 extends JComponent{
 		setPreferredSize(new Dimension(1024, 640));
 		setVisible(true);
 		
-		addKeyListener(new KeyAdapter() {
-
+		addListener();
+	}
+	
+	private void addListener(){
+		this.addMouseListener(new MouseAdapter() {
+			
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				if (e.getButton() == 1) {
+					InputUtility.setMouseLeftDown(false);
+				}
+			}
+			
+			@Override
+			public void mousePressed(MouseEvent e) {
+				if (e.getButton() == 1) {
+					InputUtility.setMouseLeftDown(true);
+					InputUtility.setMouseLeftTriggered(true);
+				}
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				InputUtility.setMouseOnScreen(false);				
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				InputUtility.setMouseOnScreen(true);
+			}
+		});
+		
+		
+		this.addMouseMotionListener(new MouseMotionListener() {
+			
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				if (InputUtility.isMouseOnScreen()) {
+					InputUtility.setMouseX(e.getX());
+					InputUtility.setMouseY(e.getY());
+				}
+			}
+			
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				if (InputUtility.isMouseOnScreen()) {
+					InputUtility.setMouseX(e.getX());
+					InputUtility.setMouseY(e.getY());
+				}
+				
+			}
+		});
+		
+		this.addKeyListener(new KeyAdapter() {
+			
 			@Override
 			public void keyReleased(KeyEvent e) {
 				InputUtility.setKeyPressed(e.getKeyCode(), false);
@@ -31,8 +87,11 @@ public class GameScreen1 extends JComponent{
 			
 			@Override
 			public void keyPressed(KeyEvent e) {
-				InputUtility.setKeyPressed(e.getKeyCode(), true);
-				
+				if(!InputUtility.getKeyPressed(e.getKeyCode())){
+					
+					InputUtility.setKeyPressed(e.getKeyCode(), true);
+					InputUtility.setKeyTriggered(e.getKeyCode(), true);
+				}
 			}
 		});
 	}
