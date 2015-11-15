436236a24cf6a44581e5d8ae23d4885195a7d7e4
Korrawe Karunratanakul <korrawe@gmail.com>
2015-11-15 20:40:47 +0700

    Added onScreenAttack and method to calculate

--- Dragoniquarium/src/logic/GameLogic.java
+++ Dragoniquarium/src/logic/GameLogic.java
@@ -1,20 +1,26 @@
 package logic;
 
+import java.awt.event.KeyEvent;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+
+//import lib.DrawingUtility;
+import input.InputUtility;
 import render.RenderableHolder;
-import render.Resource;
 
 public class GameLogic {
 	
 	private static GameLogic instance;
+	private PlayerStatus player;
 	protected List <TargetObject> onScreenObject =  new CopyOnWriteArrayList<TargetObject>();
+	protected List <AttackObject> onScreenAttack = new CopyOnWriteArrayList<AttackObject>();
 	
 	private static final int SPAWN_DELAY = 100;
 	private int spawnDelayCounter ;
 	
-	public static boolean enamyOnScreen = false;
+	public static boolean enemyOnScreen = false; 
+	public DamageableObject enemy ; //MonsterObject
 	
 	public static GameLogic getInstance() {
 		return instance;
@@ -30,7 +36,7 @@ public class GameLogic {
 	
 
 	public GameLogic() {
-//		player = new Player(); 
+		player = new PlayerStatus();
 //		playerStatus = new PlayerStatus();
 //		RenderableHolder.getInstance().add(player);
 //		RenderableHolder.getInstance().add(playerStatus);
@@ -43,10 +49,29 @@ public class GameLogic {
 //		player.update();
 //		System.out.println("logic update");
 		
+		// clear destroyed object
 		for(TargetObject obj : onScreenObject) {
 			if (obj.isDestroyed()) {
 				onScreenObject.remove(obj);
 				RenderableHolder.getInstance().getRenderableList().remove(obj);
+			}
+		}
+		
+		// clear destroy attack
+		for(AttackObject obj : onScreenAttack) {
+			if (obj.isDestroyed()) {
+				onScreenAttack.remove(obj);
+				RenderableHolder.getInstance().getRenderableList().remove(obj);
+			}
+		}
+		
+		
+		// move
+		for(TargetObject obj : onScreenObject) {
+			obj.move();
+			// check out of screen
+			if (obj.x < 0 || obj.x > 1024 || obj.y < 0 || obj.y >= 900 ) {
+				obj.destroyed = true;
 			}
 		}
 		
@@ -58,19 +83,75 @@ public class GameLogic {
 					temp.layingEgg = false;
 					createEgg(temp.x, temp.y);
 				}
+			} // else if( obj instanceof)
+		}
+		
+		// attack object do damage
+		for(AttackObject obj : onScreenAttack) {
+			obj.move();
+			if(obj.attackType == 1) {
+				for(TargetObject target : onScreenObject) {
+					if(target.destroyed) continue;
+					if(target instanceof DamageableObject && target.contains(obj.x, obj.y)) {
+						((DamageableObject)target).hit(obj.getAttack());
+						
+						// if hit guardian dragon 
+						/*if(target instanceof Dragon3) {
+							obj.destroyed = true;
+							break;
+						}*/
+					}
+				}
+				
+			} else if(obj.attackType == 2) {
+				
+				if( enemy.contains(obj.x, obj.y)) {
+					// TODO
+					obj.destroyed = true;
+				}
 			}
 		}
 		
-		for(TargetObject obj : onScreenObject) {
-			obj.move();
-			if (obj.y >= 900 ) {
-				obj.destroyed = true;
+		// attack object type 1 is clicked
+		// check shoot and grab
+		TargetObject target = null;
+//		TargetObject grabbedObject = null;
+		if(!player.isDisplayingArea(InputUtility.getMouseX(), InputUtility.getMouseY())){
+			boolean shoot = false;
+			if(input.InputUtility.isMouseLeftClicked() ){
+				shoot = true;
+			}
+			
+			target = getTopMostTargetAt(InputUtility.getMouseX(), InputUtility.getMouseY());
+			if(target != null){
+				
+				if(target instanceof CollectibleObject && shoot)	{
+					((CollectibleObject)target).grab(player);
+				}
+				else if(target instanceof AttackObject && shoot) {
+					((AttackObject)target).hitByPlayer();
+				}
+				//else if(  ) // target insance of Monster
+				
+			} else if (enemyOnScreen && shoot) {
+				// shoot animation
+//				onScreenAnimation.add(DrawingUtility.createShootingAnimationAt(
+//				input.InputUtility.getMouseX(), input.InputUtility.getMouseY()));
 			}
 		}
-		// create egg
-		// attack
+		
+		// dragon attack
 		// update fire ball
+		
+		
+		
 		spawnDelayCounter++;
+		if (spawnDelayCounter >= SPAWN_DELAY ) {
+			AttackObject atk = new AttackObject(RandomUtility.random(300, 700), 200, 10, 
+										zCounter, 1, RandomUtility.random(300, 700), 600, 3, 1);
+			onScreenAttack.add(atk);
+			RenderableHolder.getInstance().add(atk);
+		}
 		if (spawnDelayCounter >= SPAWN_DELAY ) {
 			spawnDelayCounter = 0;
 //			TargetObject egg = new Egg1(RandomUtility.random(0, 1000), 600, 10);
@@ -81,10 +162,64 @@ public class GameLogic {
 		
 	}
 	
+	private TargetObject getTopMostTargetAt(int x,int y){
+		TargetObject obj = null;
+		// find attack first
+		for(TargetObject target : onScreenAttack){
+			if(target.contains(x, y)){
+				if(obj != null){
+					if(target.getZ() > obj.getZ()){
+						obj.setPointerOver(false);
+						obj = target;
+						obj.setPointerOver(true);
+					}
+				}else{
+					obj = target;
+					obj.setPointerOver(true);
+				}
+			}else{
+				target.setPointerOver(false);
+			}
+		}
+		if(obj != null) {
+			return obj;
+		}
+		// find egg
+		for(TargetObject target : onScreenObject){
+			if(target instanceof Egg1 && target.contains(x, y)){
+				if(obj != null){
+					if(target.getZ() > obj.getZ()){
+						obj.setPointerOver(false);
+						obj = target;
+						obj.setPointerOver(true);
+					}
+				}else{
+					obj = target;
+					obj.setPointerOver(true);
+				}
+			}else{
+				target.setPointerOver(false);
+			}
+		}
+		if(obj != null || !enemyOnScreen) {
+			return obj;
+		}
+		// find enemy
+		TargetObject target = enemy;
+		if(target.contains(x, y)){
+			obj = target;
+			obj.setPointerOver(true);
+		} else{
+			target.setPointerOver(false);
+		}
+		return obj;
+	}
+	
 	public void createEgg(double x, double y) {
 		TargetObject egg = new Egg1((int)x, (int)y, zCounter);
 		onScreenObject.add(egg);
 		RenderableHolder.getInstance().add(egg);
 	}
+	
 	
 }
