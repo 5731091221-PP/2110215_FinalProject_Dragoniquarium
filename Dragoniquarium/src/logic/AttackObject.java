4e5541e7dbfab4f47df148b2ebf9e5cbc426d72a
Korrawe Karunratanakul <korrawe@gmail.com>
2015-11-15 20:37:42 +0700

    implemented Attack Object

--- Dragoniquarium/src/logic/AttackObject.java
+++ Dragoniquarium/src/logic/AttackObject.java
@@ -7,16 +7,31 @@ public class AttackObject extends TargetObject {
 
 	
 	private int attack;
+	private int life = 3;
 	private double speed;
 	
+	public int attackType;
+	/*
+	 * type 1 : dragon to monster
+	 * type 2 : monster to dragon
+	 */
+	
 	public AttackObject(double x, double y, int radius, int z, int attack, 
-			double xDes, double yDes, double speed) {
+			double xDes, double yDes, double speed, int attackType) {
 		super(x, y, radius, z);
 		this.attack = attack;
 		this.speed = speed;
+		this.attackType = attackType;
 		this.xDestination = xDes;
 		this.yDestination = yDes;
 		generateSpeed();
+	}
+	
+	public void hitByPlayer() {
+		life--;
+		if(life <= 0) {
+			destroyed = true;
+		}
 	}
 	
 	public int getAttack() {
@@ -39,8 +54,8 @@ public class AttackObject extends TargetObject {
 	
 	@Override
 	public void draw(Graphics2D g2d) {
-		g2d.setColor(Color.RED);
-		g2d.fillOval((int)(x-radius), (int)(y-radius), 10, 10);
+		g2d.setColor(Color.ORANGE);
+		g2d.fillOval((int)(x-radius), (int)(y-radius), 2*radius, 2*radius);
 		
 	}
 	
